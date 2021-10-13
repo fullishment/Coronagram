7333110be1363bf6974391eff00681d156875ab6
이혜주 <haejulee.hjl@gmail.com>
2021-10-13 17:09:00 +0900

    co_news.jsp co_new.js co_news.css co_news img

---
+++ Coronagram/src/main/webapp/resources/script/co_news/co_news.js
@@ -0,0 +1,22 @@
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%";
+}
+var acc = document.getElementsByClassName("scQ");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("SCactive");
+		var scA = this.nextElementSibling;
+		if (scA.style.display === "block") {
+			scA.style.display = "none";
+		} else {
+			scA.style.display = "block";
+		}
+	});
+}
